ca60a0d78d850eb0e96c850b28021fda78d51919
Richard Peters <richard.peters@uni-jena.de>
2024-04-01 11:43:44 +0200

    p

--- components/index/hero.tsx
+++ components/index/hero.tsx
@@ -14,7 +14,7 @@ export default function HeroSection() {
             <div>
               <div>
                 <div className="text-3xl font-bold tracking-tight sm:text-center sm:text-7xl sm:text-nowrap text-green-100">
-                  Produktionsgenossenschaft Hanf - Lichtstadt
+                  Produktionsgenossenschaft Hanf - Stadt Jena
                 </div>
                 <div className="mt-8 flex gap-x-4 sm:justify-center">
                   <button onClick={handleClick} className='ml-2 text-white px-4 py-2 text-base font-medium rounded bg-red-900 hover:bg-red-700 transition'>
